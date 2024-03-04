4c28e66c2ee7968b66720d3fc2ab17d6bac8ccf6
eugenmihailescux@gmail.com <eugenmihailescux@gmail.com>
2024-03-04 05:29:25 +0100

    prepended input icon

--- components/ui/password-input.tsx
+++ components/ui/password-input.tsx
@@ -1,9 +1,16 @@
+import { cn } from '@/lib/utils';
 import React, { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { InputProps, Input } from './input';
+import { GoKey } from 'react-icons/go';
+import { Input, InputProps } from './input';
 
-const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
-  (props, ref) => {
+export interface PasswordInputProps extends InputProps {
+  showKeyIcon?: boolean;
+  passwordVisibilityToogle?: boolean;
+}
+
+const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
+  ({ showKeyIcon = true, passwordVisibilityToogle = true, ...props }, ref) => {
     const [showPassword, setShowPassword] = useState(false);
 
     const togglePasswordVisibility = () => {
@@ -13,18 +20,27 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div className="relative">
         <Input
+          className={cn(
+            showKeyIcon ? 'pl-10' : '',
+            passwordVisibilityToogle ? 'pr-10' : '',
+          )}
           autoComplete="off"
           ref={ref}
           {...props}
           type={showPassword ? 'text' : 'password'}
         />
-        <button
-          type="button"
-          onClick={togglePasswordVisibility}
-          className="absolute right-4 top-1/2 -translate-y-1/2 transform focus:outline-none"
-        >
-          {showPassword ? <FaEyeSlash /> : <FaEye />}
-        </button>
+        {showKeyIcon && (
+          <GoKey className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        )}
+        {passwordVisibilityToogle && (
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute right-4 top-1/2 -translate-y-1/2 transform focus:outline-none"
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        )}
       </div>
     );
   },
